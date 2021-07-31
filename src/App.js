251449276778e328a583e672d4c53ac2bfe73171
CarlMartins <lemos_oliveiraa@hotmail.com>
2021-07-31 12:01:45 -0300

    Renderizacao condicional

--- src/App.js
+++ src/App.js
@@ -1,11 +1,40 @@
 import React, { Component } from 'react';
-import Membro from './components/Membro';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      status: true
+    };
+
+    this.sair = this.sair.bind(this);
+    this.entrar = this.entrar.bind(this);
+  }
+
+  sair() {
+    this.setState({ status: false });
+  }
+
+  entrar() {
+    this.setState({ status: true });
+  }
+
   render() {
     return (
       <div>
-        <Membro nome="Visitante" />
+        {
+          this.state.status ?
+            <div>
+              <h2>Bem vindo ao sistema</h2>
+              <button onClick={this.sair}>Entrar no sistema</button>
+            </div>
+            :
+            <div>
+              <h2>Olá visitante, faça login!</h2>
+              <button onClick={this.entrar}>Sair</button>
+            </div>
+        }
       </div>
     );
   }
